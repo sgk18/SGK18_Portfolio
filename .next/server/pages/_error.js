07151255c525bc0c1c0951268e29bd8ef278d830
Suryachalam V M <suryachalam18@gmail.com>
2026-02-10 23:00:57 +0530

    next

---
+++ .next/server/pages/_error.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
+R.c("server/chunks/ssr/node_modules_next_55786baf._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__74858115._.js")
+R.c("server/chunks/ssr/[externals]_react_jsx-runtime_7ad7b9a3._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__2574aea5._.js")
+R.c("server/chunks/ssr/pages__app_43455d5d.js")
+R.m(10910)
+module.exports=R.m(10910).exports
