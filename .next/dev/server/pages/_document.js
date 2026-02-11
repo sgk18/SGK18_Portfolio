6c6511d65712f05f824deb295f4ee37918ef5987
Suryachalam V M <suryachalam18@gmail.com>
2026-02-11 22:01:13 +0530

    fixes

--- .next/dev/server/pages/_document.js
+++ .next/dev/server/pages/_document.js
@@ -1,5 +1,4 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/node_modules_186c80ea._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__13628319._.js")
 R.m("[project]/node_modules/next/document.js [ssr] (ecmascript)")
 module.exports=R.m("[project]/node_modules/next/document.js [ssr] (ecmascript)").exports
--- .next/dev/static/chunks/pages/index.js
+++ .next/dev/static/chunks/pages/index.js
@@ -1,14 +1,10 @@
 __turbopack_load_page_chunks__("/", [
   "static/chunks/[root-of-the-server]__2d716426._.js",
-  "static/chunks/node_modules_next_dist_compiled_8ca6b690._.js",
-  "static/chunks/node_modules_next_dist_shared_lib_9a2a7093._.js",
-  "static/chunks/node_modules_next_dist_client_5a8a528e._.js",
-  "static/chunks/node_modules_next_dist_75b597d7._.js",
+  "static/chunks/node_modules_next_dist_compiled_6a713666._.js",
+  "static/chunks/node_modules_next_dist_cf28f829._.js",
   "static/chunks/node_modules_next_head_6b89d279.js",
-  "static/chunks/node_modules_react-dom_4411d9bd._.js",
   "static/chunks/node_modules_tsparticles-engine_esm_06aa6aed._.js",
-  "static/chunks/node_modules_@tsparticles_engine_browser_7e414d00._.js",
-  "static/chunks/node_modules_64b82cfd._.js",
+  "static/chunks/node_modules_4d8bf9b6._.js",
   "static/chunks/pages_index_2da965e7._.js",
-  "static/chunks/turbopack-pages_index_14b7916c._.js"
+  "static/chunks/turbopack-pages_index_e699f078._.js"
 ])
