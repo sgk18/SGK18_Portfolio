3fd529aeb1d7960615b48e1aa911b59a6b98089f
Suryachalam V M <suryachalam18@gmail.com>
2026-02-11 21:46:53 +0530

    next js

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,31 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/": [
-      "static/chunks/[root-of-the-server]__2d716426._.js",
-      "static/chunks/node_modules_next_dist_compiled_8ca6b690._.js",
-      "static/chunks/node_modules_next_dist_shared_lib_9a2a7093._.js",
-      "static/chunks/node_modules_next_dist_client_5a8a528e._.js",
-      "static/chunks/node_modules_next_dist_75b597d7._.js",
-      "static/chunks/node_modules_next_head_6b89d279.js",
-      "static/chunks/node_modules_react-dom_4411d9bd._.js",
-      "static/chunks/node_modules_tsparticles-engine_esm_06aa6aed._.js",
-      "static/chunks/node_modules_@tsparticles_engine_browser_7e414d00._.js",
-      "static/chunks/node_modules_64b82cfd._.js",
-      "static/chunks/pages_index_2da965e7._.js",
-      "static/chunks/turbopack-pages_index_14b7916c._.js"
-    ],
-    "/_app": [
-      "static/chunks/node_modules_next_dist_compiled_8ca6b690._.js",
-      "static/chunks/node_modules_next_dist_shared_lib_8bac8163._.js",
-      "static/chunks/node_modules_next_dist_client_5a8a528e._.js",
-      "static/chunks/node_modules_next_dist_75b597d7._.js",
-      "static/chunks/node_modules_react-dom_4411d9bd._.js",
-      "static/chunks/node_modules_f1be3ba2._.js",
-      "static/chunks/[root-of-the-server]__01feb856._.js",
-      "static/chunks/styles_style_9322a358.css",
-      "static/chunks/pages__app_2da965e7._.js",
-      "static/chunks/turbopack-pages__app_5e2481b0._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "polyfillFiles": [],
